ec1d1c28617df8990900eb7ff5a490745e1ad836
Oleksandr-1986 <73231857+Oleksandr-1986@users.noreply.github.com>
2020-12-17 11:10:29 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+﻿//burgerMenu
+
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger,.header-top__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+$('.video__icon').click(function () {
+	$(this).hide();
+	$('.video-yt').attr('src', 'https://www.youtube.com/embed/AWKzr6n0ea0?autoplay=1');
+});
+//topBtn
+
+$(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$('.topBtn').fadeIn();
+		} else {
+			$('.topBtn').fadeOut();
+		}
+	});
+	$('.topBtn').click(function () {
+		$('body,html').animate({ scrollTop: 0 }, 2000);
+	});
+});
